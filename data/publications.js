0b0412289f6c421ffe95b6c599eb7b6d7079f762
Leezzyy <107088432+Leezzyy@users.noreply.github.com>
2025-05-12 13:32:48 +0100

    publications.js

--- data/publications.js
+++ data/publications.js
@@ -1,22 +1,21 @@
-define({
-  entries: {
-    "Raza2020OnlineShopping": {
-      "title": "Consumer Preferences Towards Online Shopping",
-      "author": "Raza, Umer and Qazi, Makhdoom",
-      "year": "2020",
-      "journal": "Journal of Retailing and Consumer Services",
-      "doi": "10.1016/j.jretconser.2019.102019",
-      "abstract": "This paper explores consumer preferences in online shopping behavior...",
-      "keywords": "type:analysis, application:e-commerce, interval_data:true"
-    },
-    "Hu2023MarketAnalysis": {
-      "title": "Customer Market Analysis Based on Interval-Valued Data",
-      "author": "Hu",
-      "year": "2023",
-      "journal": "Conference on Electronic Technology",
-      "doi": "10.1109/ICETC.2010.5529640",
-      "abstract": "Interval-valued data provides a richer representation of customer preferences...",
-      "keywords": "type:market_analysis, method:interval_data, application:consumer"
+define({ entries : {
+    "Abello2013Modular": {
+        "abstract": "Large dynamic networks are targets of analysis in many fields. Tracking temporal changes at scale in these networks is challenging due in part to the fact that small changes can be missed or drowned-out by the rest of the network. For static networks, current approaches allow the identification of specific network elements within their context. However, in the case of dynamic networks, the user is left alone with finding salient local network elements and tracking them over time. In this work, we introduce a modular DoI specification to flexibly define what salient changes are and to assign them a measure of their importance in a time-varying setting. The specification takes into account neighborhood structure information, numerical attributes of nodes/edges, and their temporal evolution. A tailored visualization of the DoI specification complements our approach. Alongside a traditional node-link view of the dynamic network, it serves as an interface for the interactive definition of a DoI function. By using it to successively refine and investigate the captured details, it supports the analysis of dynamic networks from an initial view until pinpointing a user's analysis goal. We report on applying our approach to scientific co-authorship networks and give concrete results for the DBLP dataset.",
+        "address": "Los Alamitos, CA, USA",
+        "author": "Abello, James and Hadlak, Steffen and Schumann, Heidrun and Schulz, Hans-J\\\"org",
+        "doi": "10.1109/TVCG.2013.109",
+        "issn": "1077-2626",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:technique, paradigm:node-link, time:animation, evaluation:case\\_study, application:document, force-directed\\_layout, network\\_metrics, online_problem, special-purpose_layout, compound_graph",
+        "number": "3",
+        "pages": "337--350",
+        "publisher": "IEEE Computer Society",
+        "references": "Farrugia2011Effective Frishman2008Online Reitz2009Focused",
+        "series": "TVCG",
+        "title": "A Modular Degree-of-Interest Specification for the Visual Analysis of Large Dynamic Networks",
+        "type": "article",
+        "volume": "20",
+        "year": "2013"
     }
   }
 });
