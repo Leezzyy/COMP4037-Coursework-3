05535c98942e259732cec47ef7aa9883892ea98b
Leezzyy <107088432+Leezzyy@users.noreply.github.com>
2025-05-15 00:52:55 +0100

    Update available_img.js

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -2,11 +2,11 @@ const availableImg = [
   "Raza2024Consumer",
   "Hu2023Customer",
   "Durso2023Fuzzy",
-  "Li2022FeatureSelection",  // 注意：你的原文是Li2022FeatureSelection，不是Li2023IntervalDominance
+  "Li2022FeatureSelection",  
   "Comas2025FuzzyPredicate",
-  "Ma2024MultiviewClassification", // 注意：你的原文是Ma2024MultiviewClassification，不是Ma2025Multiview
+  "Ma2024MultiviewClassification", 
   "Kabir2017Overlap",
-  "Huanghua2010Attribute", // 注意：你的原文是Huanghua2010Attribute，不是Huanghua2010SVM
-  "Gunn2020FuzzySimilarity", // 注意：你的原文是Gunn2020FuzzySimilarity，不是Gunn2020IAA
+  "Huanghua2010Attribute", 
+  "Gunn2020FuzzySimilarity",
   "Cross2020SentenceSimilarity"
 ];
