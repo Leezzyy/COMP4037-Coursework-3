f6af28a6c96e8bc16c2adf4ce2138593e68a3728
Leezzyy <107088432+Leezzyy@users.noreply.github.com>
2025-05-14 01:37:50 +0100

    bib.js

--- data/generated/bib.js
+++ data/generated/bib.js
@@ -1,17 +1,102 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
+const generatedBibEntries = {
+  "Huanghua2024Consumer": {
+    "author": "Huanghua, J. and Dexian, Z.",
+    "title": "Consumer Preferences Towards Online Shopping of Electronic Products",
+    "doi": "10.1109/IIPEM62726.2024.10925701",
+    "journal": "Proceedings of the 2024 International Conference on Innovative Intelligent Industrial Production and Engineering Management (IIPEM)",
+    "publisher": "IEEE",
+    "type": "conference",
+    "year": "2024",
+    "url": "https://doi.org/10.1109/IIPEM62726.2024.10925701"
+  },
+  "Hu2023Customer": {
+    "author": "Hu, H.",
+    "title": "Customer Market Analysis Based on Interval Value Data Dynamic Clustering Algorithm",
+    "doi": "10.1109/ICIICS59993.2023.10421290",
+    "journal": "Proceedings of the 2023 International Conference on Intelligent Information and Communication Systems (ICIICS)",
+    "publisher": "IEEE",
+    "type": "conference",
+    "year": "2023",
+    "url": "https://doi.org/10.1109/ICIICS59993.2023.10421290"
+  },
+  "Durso2023Fuzzy": {
+    "author": "D’Urso, P., De Giovanni, L., Federico, L., and Vitale, V.",
+    "title": "Fuzzy Clustering of Spatial Interval-Valued Data",
+    "doi": "10.1016/j.spasta.2023.100764",
+    "journal": "Spatial Statistics",
+    "publisher": "Elsevier",
+    "type": "article",
+    "year": "2023",
+    "url": "https://doi.org/10.1016/j.spasta.2023.100764"
+  },
+  "Li2022Interval": {
+    "author": "Li, W., Zhou, H., Xu, W., Wang, X.-Z., and Pedrycz, W.",
+    "title": "Interval Dominance-Based Feature Selection for Interval-Valued Ordered Data",
+    "doi": "10.1109/TNNLS.2022.3184120",
+    "journal": "IEEE Transactions on Neural Networks and Learning Systems",
+    "publisher": "IEEE",
+    "type": "article",
+    "year": "2022",
+    "url": "https://doi.org/10.1109/TNNLS.2022.3184120"
+  },
+  "Comas2025Interval": {
+    "author": "Comas, D. S., Meschino, G. J., and Ballarin, V. L.",
+    "title": "Interval-valued fuzzy predicates from labeled data: An approach to data classification and knowledge discovery",
+    "doi": "10.1016/j.ins.2025.122033",
+    "journal": "Information Sciences",
+    "publisher": "Elsevier",
+    "type": "article",
+    "year": "2025",
+    "url": "https://doi.org/10.1016/j.ins.2025.122033"
+  },
+  "Ma2024Multiview": {
+    "author": "Ma, G., Lu, J., Fang, Z., Liu, F., and Zhang, G.",
+    "title": "Multiview Classification Through Learning From Interval-Valued Data",
+    "doi": "10.1109/TNNLS.2024.3421657",
+    "journal": "IEEE Transactions on Neural Networks and Learning Systems",
+    "publisher": "IEEE",
+    "type": "article",
+    "year": "2024",
+    "url": "https://doi.org/10.1109/TNNLS.2024.3421657"
+  },
+  "Kabir2017Novel": {
+    "author": "Kabir, S., Wagner, C., Havens, T. C., Anderson, D. T., and Aickelin, U.",
+    "title": "Novel Similarity Measure for Interval-Valued Data Based on Overlapping Ratio",
+    "doi": "10.1109/FUZZ-IEEE.2017.8015623",
+    "journal": "Proceedings of the 2017 IEEE International Conference on Fuzzy Systems (FUZZ-IEEE)",
+    "publisher": "IEEE",
+    "type": "conference",
+    "year": "2017",
+    "url": "https://doi.org/10.1109/FUZZ-IEEE.2017.8015623"
+  },
+  "Huanghua2010Research": {
+    "author": "Huanghua, H. and Dexian, Z.",
+    "title": "Research on Attribute Interval Optimization Method for Segmentation Based SVM and the Decision Tree Learning",
+    "doi": "10.1109/ICETC.2010.5529640",
+    "journal": "Proceedings of the 2010 2nd International Conference on Education Technology and Computer (ICETC)",
+    "publisher": "IEEE",
+    "type": "conference",
+    "year": "2010",
+    "url": "https://doi.org/10.1109/ICETC.2010.5529640"
+  },
+  "Gunn2020Similarity": {
+    "author": "Gunn, J. K., Khorshidi, H. A., and Aickelin, U.",
+    "title": "Similarity Measure for Aggregated Fuzzy Numbers from Interval-Valued Data",
+    "doi": "10.1016/j.socl.2020.100002",
+    "journal": "Soft Computing Letters",
+    "publisher": "Elsevier",
+    "type": "article",
+    "year": "2020",
+    "url": "https://doi.org/10.1016/j.socl.2020.100002"
+  },
+  "Cross2020Using": {
+    "author": "Cross, V., Mokrenko, V., Crockett, K., and Adel, N.",
+    "title": "Using Fuzzy Set Similarity in Sentence Similarity Measures",
+    "doi": "10.1109/FUZZ48607.2020.9177836",
+    "journal": "Proceedings of the 2020 IEEE International Conference on Fuzzy Systems (FUZZ-IEEE)",
+    "publisher": "IEEE",
+    "type": "conference",
+    "year": "2020",
+    "url": "https://doi.org/10.1109/FUZZ48607.2020.9177836"
+  }
 };
