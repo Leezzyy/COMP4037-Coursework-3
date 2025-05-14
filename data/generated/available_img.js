d3d36fa270a479c9cd7ebe323ca9baa809581152
Leezzyy <107088432+Leezzyy@users.noreply.github.com>
2025-05-15 00:05:22 +0100

    Update available_img.js

--- data/generated/available_img.js
+++ data/generated/available_img.js
@@ -1,1 +1,12 @@
-const availableImg = ["Beck2016Visual"];
+const availableImg = [
+  "Raza2024Consumer",
+  "Hu2023Customer",
+  "Durso2023Fuzzy",
+  "Li2023IntervalDominance",
+  "Comas2025FuzzyPredicate",
+  "Ma2025Multiview",
+  "Kabir2017Overlap",
+  "Huanghua2010SVM",
+  "Gunn2020IAA",
+  "Cross2020SentenceSimilarity"
+];
